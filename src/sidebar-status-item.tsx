8fd23f7b3ae6a24931982aae84bbb7fa42436b5a
Manuel Steiner <manuel.steiner@mts-international.org>
2022-06-06 10:36:42 +0200

    update sidebar status icon to feather

--- src/sidebar-status-item.tsx
+++ src/sidebar-status-item.tsx
@@ -28,51 +28,18 @@ const SidebarStatusItem = () => {
       indentLevel={0}
       renderIcon={() => (
         <svg
+          xmlns="http://www.w3.org/2000/svg"
           className={
-            'streamline-icon' +
+            'feather-icon' +
             (syncStatus === SYNC_STATUS.ACTIVE ? ' animate-spin' : '')
           }
           width="1em"
           height="1em"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 600 600"
+          viewBox="0 0 24 24"
         >
-          <g transform="matrix(42.857142857142854,0,0,42.857142857142854,0,0)">
-            <g>
-              <g>
-                <polyline
-                  points="11 9 13 8.5 13.5 10.5"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></polyline>
-                <path
-                  d="M13,8.5A6.76,6.76,0,0,1,7,13H7A6,6,0,0,1,1.36,9.05"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-              </g>
-              <g>
-                <polyline
-                  points="3 5 1 5.5 0.5 3.5"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></polyline>
-                <path
-                  d="M1,5.5C1.84,3.2,4.42,1,7,1H7a6,6,0,0,1,5.64,4"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-              </g>
-            </g>
-          </g>
+          <polyline points="23 4 23 10 17 10"></polyline>
+          <polyline points="1 20 1 14 7 14"></polyline>
+          <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path>
         </svg>
       )}
     >
